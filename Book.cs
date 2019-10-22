fa2014390e743f532c5adb2db582ad2e1cd4f0ad
Mohammad Khattab <MOHAMMAD.KHATTAB.987@gmail.com>
2019-10-22 15:24:39 +0200

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Book
+    {
+        public Book(string name)
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
+         
+       public void addgrade (double garde)
+        {
+            grades.Add(garde);
+        }
+
+       private  List<double> grades ;
+        private string name;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("scott's Grade Book");
+            book.addgrade(89.1);
+            book.addgrade(90.5);
+            book.addgrade(77.5);
+
+            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            grades.Add(56.1);
+
+            var result = 0.0;
+            var highgarde = double.MinValue;
+                   
+            foreach (var  number in grades)
+            {
+                if (number > highgarde)
+                {
+                    highgarde = number;
+                }
+               
+                result += number ;
+                result /= grades.Count;
+            }
+            Console.WriteLine($"The avrage grade For is { result:n1}");
+            Console.WriteLine($"The highest grade  is { highgarde}");
+
+
+        }
+    }
+}
